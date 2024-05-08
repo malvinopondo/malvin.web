bdb55553f72ec8cf0fe36b5e61facbbcaed61ab3
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-05-08 11:26:37 +0300

    feat(Skills): Import the skills component inside the app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { MailchimpForm } from './components/MailchimpForm';
 import Newsletter from './components/Newsletter';
 import { ProjectCard } from './components/ProjectCard';
 import Projects from "./components/Projects";
+import { Skills } from './components/Skill';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <Newsletter />
       <ProjectCard />
       <Projects />
+      <Skills />
 
     </div>
       
