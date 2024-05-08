46dedfc8f8558134ae4f78c769ddcf52b8ce8c5b
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-05-08 11:08:04 +0300

    feat(MailChimp): Add the mailchimp component and return it in the app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
 
 function App() {
   return (
